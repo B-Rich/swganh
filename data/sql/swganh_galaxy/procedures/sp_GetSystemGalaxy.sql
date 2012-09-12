05ee3c237599d40af9678dd32071cf21e68712ac
Aleksandar Todorovic <atodorovic@gmail.com>
2012-09-12 01:30:49 -0400

    new stored procedures for galaxy_manager
    new stored procedures for galaxy_manager

---
+++ data/sql/swganh_galaxy/procedures/sp_GetSystemGalaxy.sql
@@ -0,0 +1,36 @@
+﻿/*
+---------------------------------------------------------------------------------------
+This source file is part of SWG:ANH (Star Wars Galaxies - A New Hope - Server Emulator)
+
+For more information, visit http://www.swganh.com
+
+Copyright (c) 2006 - 2012 The SWG:ANH Team
+---------------------------------------------------------------------------------------
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+---------------------------------------------------------------------------------------
+*/
+
+use swganh_galaxy;
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `swganh_galaxy`.`sp_GetSystemGalaxy` $$
+CREATE PROCEDURE `sp_GetSystemGalaxy`()
+BEGIN
+
+  -- grab our galaxy details
+  SELECT * FROM swganh_galaxy_manager.galaxy ORDER BY name;
+
+END $$
---
+++ data/sql/swganh_galaxy/procedures/sp_GetSystemIntangible.sql
@@ -0,0 +1,35 @@
+﻿/*
+---------------------------------------------------------------------------------------
+This source file is part of SWG:ANH (Star Wars Galaxies - A New Hope - Server Emulator)
+
+For more information, visit http://www.swganh.com
+
+Copyright (c) 2006 - 2012 The SWG:ANH Team
+---------------------------------------------------------------------------------------
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+---------------------------------------------------------------------------------------
+*/
+
+use swganh_galaxy;
+
+DELIMITER $$
+
+CREATE PROCEDURE `sp_GetSystemIntangible`(IN object_id BIGINT)
+BEGIN
+
+  SELECT * FROM v_objects WHERE id = object_id;
+  SELECT * FROM v_intangibles WHERE id = object_id;
+
+END $$
