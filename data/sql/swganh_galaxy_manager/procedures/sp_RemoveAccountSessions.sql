e509eced88edffee83864d7ff94110b050a9de75
Aleksandar Todorovic <atodorovic@gmail.com>
2012-09-14 02:28:39 -0400

    swganh_galaxy_manager
    - sp_RemoveAccountSessions()

---
+++ data/sql/swganh_galaxy_manager/procedures/sp_RemoveAccountSessions.sql
@@ -0,0 +1,17 @@
+﻿/* 
+	This file is part of SWGANH which is released under the MIT license.
+	See file LICENSE or go to http://swganh.com/LICENSE
+*/
+
+USE swganh_galaxy_manager;
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `swganh_galaxy_manager`.`sp_RemoveAccountSessions` $$
+CREATE PROCEDURE `sp_RemoveAccountSessions`()
+BEGIN
+
+  -- grab our galaxy population via sessions table
+  TRUNCATE swganh_galaxy_manager.sessions;
+
+END $$
