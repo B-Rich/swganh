e1a001e06dbf8cd8c8631103ba23092c9af11ae7
Aleksandar Todorovic <atodorovic@gmail.com>
2012-09-14 01:32:51 -0400

    swganh_galaxy
    - sp_GetSystemObjectList()

---
+++ data/sql/swganh_galaxy/procedures/sp_GetSystemObjectList.sql
@@ -0,0 +1,39 @@
+﻿/*
+---------------------------------------------------------------------------------------
+This source file is part of SWG:ANH (Star Wars Galaxies - A New Hope - Server Emulator)
+
+For more information, visit http://www.swganh.com
+
+Copyright (c) 2006 - 2012 The SWG:ANH Team
+---------------------------------------------------------------------------------------
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+---------------------------------------------------------------------------------------
+*/
+
+use swganh_galaxy;
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `swganh_galaxy`.`sp_GetSystemObjectList` $$
+CREATE PROCEDURE `swganh_galaxy`.`sp_GetSystemObjectList` ()
+BEGIN
+  SELECT
+    objects.object_string,
+    object_types.object_type
+  FROM
+    swganh_static.objects
+  INNER JOIN swganh_static.object_types ON (swganh_static.objects.object_type = swganh_static.object_types.id);
+
+END $$
