1c7cf5a59feea3c134bf176e24402e7571f75333
Aleksandar Todorovic <atodorovic@gmail.com>
2012-09-14 02:32:51 -0400

    swganh_galaxy_manager
    - renamed sp(s)
    - added new sp(s)

---
+++ data/sql/swganh_galaxy_manager/procedures/sp_GetAccountBySessionID.sql
@@ -0,0 +1,17 @@
+﻿/* 
+	This file is part of SWGANH which is released under the MIT license.
+	See file LICENSE or go to http://swganh.com/LICENSE
+*/
+
+USE swganh_galaxy_manager;
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `swganh_galaxy_manager`.`sp_GetAccountBySessionID` $$
+CREATE PROCEDURE `sp_GetAccountBySessionID`(IN s_key VARCHAR(255))
+BEGIN
+
+  -- grab our galaxy population via sessions table
+  SELECT account_id FROM swganh_galaxy_manager.sessions WHERE session = s_key;
+
+END $$
