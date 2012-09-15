1ec2c02464a50b0f6cfb16cb7fd5cb50820acd93
Aleksandar Todorovic <atodorovic@gmail.com>
2012-09-14 22:07:32 -0400

    fix
    fix

--- data/sql/swganh_galaxy_manager/procedures/sp_UpdateGalaxyService.sql
+++ data/sql/swganh_galaxy_manager/procedures/sp_UpdateGalaxyService.sql
@@ -10,9 +10,5 @@ DROP PROCEDURE IF EXISTS `sp_UpdateGalaxyService` $$
 
 CREATE PROCEDURE `sp_UpdateGalaxyService`(IN s_address VARCHAR(55), IN s_tcp INT, IN s_udp INT, IN s_ping INT, IN s_status INT, IN s_heartbeat DATETIME, IN s_id BIGINT)
 BEGIN
-<<<<<<< HEAD
-    UPDATE service SET service_address = INET_ATON(s_address), tcp_port = s_tcp, udp_port = s_udp, ping_port = s_ping, last_heartbeat = s_heartbeat where id = s_id;
-=======
     UPDATE service SET service_address = INET_ATON(s_address), tcp_port = s_tcp, udp_port = s_udp, ping_port = s_ping, status = s_status, last_heartbeat = s_heartbeat where id = s_id;
->>>>>>> 87494bda9c05c1c68d4710f158b6e707c975636e
 END $$
